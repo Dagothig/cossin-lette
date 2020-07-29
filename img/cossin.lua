b37e5d7dc52201f8a47a5d09083b3e3d2f82d448
dagothig <guillaumenoelmartel@gmail.com>
2020-07-29 01:26:35 -0400

    WOOOOOOOOOOOOOOOOOO

---
+++ img/cossin.lua
@@ -0,0 +1,17 @@
+return {
+    {
+        src = "cossin-neutre.png",
+        size = { 64, 96 },
+        decal = { 32, 80 },
+        fps = 4,
+        flags_y = { { 1.5 }, { 1.75 }, { 0 }, { 0.25 }, { 0.5 } }
+    },
+    {
+        src = "cossin-marche.png",
+        size = { 64, 96 },
+        decal = { 32, 80 },
+        fps = 10,
+        flags_y = { { 1.5 }, { 1.75 }, { 0 }, { 0.25 }, { 0.5 } },
+        flags = { 'moving' }
+    }
+}
